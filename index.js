7d714631631622ce852e7e68427d1abad94b589a
Piliponful <piliponful@gmail.com>
2016-09-16 10:11:40 +0300

    Added Gzip compresion, es6 syn

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 //Required modules
-var http = require('http'),
-    url = require('url'),
-    path = require('path'),
-    fs = require('fs'),
-    
-    //Array(or object) of M(multipurpose) I(internet) M(mail) E(extensions) - MIME types
-    mimeTypes = {
-    'html' : 'text/html',
-    'js'   : 'text/javascript',
-    'css'  : 'text/css',
-    'png'  : 'image/png',
-    'jpg'  : 'image/jpeg',
-    'jpeg' : 'image/jpeg'
-};
+const http = require('http'),
+      url  = require('url'),
+      path = require('path'),
+      fs   = require('fs'),
+      gzip = request('zlib').createGzip(),
 
-http.createServer(function (req, res) {
+//Array(or object) of M(multipurpose) I(internet) M(mail) E(extensions) - MIME types
+      mimeTypes = {
+        'html' : 'text/html',
+        'js'   : 'text/javascript',
+        'css'  : 'text/css',
+        'png'  : 'image/png',
+        'jpg'  : 'image/jpeg',
+        'jpeg' : 'image/jpeg'
+      };
+
+http.createServer((req, res) => {
 
     var pathName = url.parse(req.url).pathname,
         fileName = path.join(__dirname, decodeURIComponent(pathName)),
         stats;
-    
-    console.log('Loading %j', pathName);
-    
+
+    console.log(`Loading ${pathName}`);
+
     try {
         stats = fs.statSync(fileName);
     } catch(err) {
@@ -30,34 +31,33 @@ http.createServer(function (req, res) {
         res.end(err.message);
         return;
     }
-    
+
     if(stats.isFile()){
-        
+
         var mimeType = mimeTypes[path.extname(fileName).slice(1)];
-        
+
         res.writeHead(200, {'Content-type': mimeType});
         var readStream = fs.createReadStream(fileName);
-        readStream.pipe(res);
-        readStream.on('end', function () {
+        readStream.pipe(gzip).pipe(res);
+        readStream.on('end', () => {
             console.log("All data are send properly and streaming is ended. Shut down the response!");
             res.end();
-        })
-        
-        
+        });
+
     } else if(stats.isDirectory()){
-        
+
         res.writeHead(302, {
-            Location: '/index.html'
+          Location: '/index.html'
         });
         res.end();
-        
+
     } else {
         res.writeHead(500, {'Content-type': 'text/plain'});
         res.write('500 Internal Error');
         res.end();
     }
-    
-}).listen(process.env.PORT || 3000, function () {
+
+}).listen(process.env.PORT || 3000, () => {
     console.log('Server is running');
 });
 
